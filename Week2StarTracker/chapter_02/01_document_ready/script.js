aa647555deac64c9abbf727908bf577370316bd5
Lyndsie Anderson <lba2134@barnard.edu>
2014-10-08 20:08:12 -0400

    Added Hide and Toggle buttons

--- Week2StarTracker/chapter_02/01_document_ready/script.js
+++ Week2StarTracker/chapter_02/01_document_ready/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   console.log("OMG JAVASCRIPT")
- $("#celebs tbody tr:even").addClass("zebra");
+ 	$("#celebs tbody tr:even").addClass("zebra");
+
+ 	$('#hideButton').click(function(){
+ 		$('#disclaimer').hide(); 
+ 
+ 	})
+ 	$('#showButton').click(function(){
+ 		$('#disclaimer').show(); 
+ 	})
 });
