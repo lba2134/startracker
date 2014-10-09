6cfb8b821d279b276b483fdb4a77d8d0e4d93480
Lyndsie Anderson <lba2134@barnard.edu>
2014-10-08 20:45:38 -0400

    Modified toggle button
    Edited jQuery for efficiency and changed button value to show/hide upon
    click.

--- Week2StarTracker/chapter_02/01_document_ready/script.js
+++ Week2StarTracker/chapter_02/01_document_ready/script.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
   console.log("OMG JAVASCRIPT")
  	$("#celebs tbody tr:even").addClass("zebra");
 
- 	$('#toggleButton').click(function(){
- 		if ($('#disclaimer').is(':visible')) {
- 			$('#disclaimer').hide();
- 		}
- 		else {
- 			$('#disclaimer').show();
- 		}
- 	})
+ 	// $('#toggleButton').click(function(){
+ 	// 	if ($('#disclaimer').is(':visible')) {
+ 	// 		$('#disclaimer').hide();
+ 	// 	}
+ 	// 	else {
+ 	// 		$('#disclaimer').show();
+ 	// 	}
+ 	// })
+ 		$('#toggleButton').click(function(){
+ 			$("#disclaimer").toggle();
+ 			if ($('#disclaimer').is(":visible")){
+ 				$(this).val('hide');
+ 			}
+ 			else {
+ 				$(this).val('show');
+ 			}
+
+ 		})
 });
